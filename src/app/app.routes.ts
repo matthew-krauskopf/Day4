dc454db0f45e91e231421ca1ad44fac7c75f271f
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-19 11:20:56 -0400

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: PokedexComponent },
+    { path: "", component: LoginComponent },
+    { path: '**', component: NotFoundComponent }
+];
