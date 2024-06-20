d1a7424a35ac22dc8f42cae149273f553b657d98
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-20 09:58:03 -0400

    Good checkpoint for login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
