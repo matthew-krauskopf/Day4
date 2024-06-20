1bfe9bf720899ba1cb1d8c5b9c623de83f0f9eb8
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-20 10:34:06 -0400

    Add 404 page

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
